2640ab2b8e8d5c5935ceeeb83aa5eae5dd611655
checkswinga <aleemium@gmail.com>
2022-12-30 00:42:23 -0500

    Add copy/move mp4 files

--- scripts/move_videos.sh
+++ scripts/move_videos.sh
@@ -2,3 +2,6 @@
 cp -p /c/Users/Buildmaster/OneDrive/Pictures/Camera\ Roll/20*/*/*.MOV /d/Aleem/Videos/
 cp -p /c/Users/Buildmaster/OneDrive/Pictures/Camera\ Roll/20*/*/*.MOV /e/Aleem/Videos/
 mv /c/Users/Buildmaster/OneDrive/Pictures/Camera\ Roll/20*/*/*.MOV /c/Users/Buildmaster/OneDrive/Videos/
+cp -p /c/Users/Buildmaster/Downloads/*.mp4 /d/Aleem/Videos/
+cp -p /c/Users/Buildmaster/Downloads/*.mp4 /e/Aleem/Videos/
+mv /c/Users/Buildmaster/Downloads/*.mp4 /c/Users/Buildmaster/OneDrive/Videos/
