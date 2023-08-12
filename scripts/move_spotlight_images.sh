b852e28c6805b605d756a6ca8efcf17cd6f8487b
checkswinga <aleemium@gmail.com>
2023-08-12 18:37:18 -0400

    Minor update move_spotlight_images.sh

--- scripts/move_spotlight_images.sh
+++ scripts/move_spotlight_images.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd /c/Users/Buildmaster/AppData/Local/Packages/Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy/LocalState/Assets
-cp * /d/Aleem/Spotlight/
-cd /d/Aleem/Spotlight/
+cp * /d/Spotlight/
+cd /d/Spotlight/
 for i in $(ls); do mv $i $i.jpg; done
