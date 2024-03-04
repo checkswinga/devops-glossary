0cfbdefc0caf0c2f11afca65e45da1bbfca12f7d
checkswinga <aleemsjanmohamed@gmail.com>
2024-03-03 23:50:38 -0500

    Update paths on scripts for new PC

--- scripts/move_music.sh
+++ scripts/move_music.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd ~/Downloads/
-cp -p "$1" /d/Aleem/Music/$2/
-cp -p "$1" /e/Aleem/Music/$2/
-mv "$1" /c/Users/Buildmaster/OneDrive/Music/$2/
+#cp -p "$1" /d/Aleem/Music/$2/
+#cp -p "$1" /e/Aleem/Music/$2/
+mv "$1" /c/Users/aleem/OneDrive/Music/$2/
--- scripts/move_wallpaper.sh
+++ scripts/move_wallpaper.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd ~/Downloads/
-cp -p "$1"* /d/Aleem/Wallpapers/
-cp -p "$1"* /e/Aleem/Wallpapers/
-mv "$1"* /c/Users/Buildmaster/OneDrive/Wallpapers/
+#cp -p "$1"* /d/Aleem/Wallpapers/
+#cp -p "$1"* /e/Aleem/Wallpapers/
+mv "$1"* /c/Users/aleem/OneDrive/Wallpapers/
